f09473004766e5cf844ac428dad3ca03e31dc403
rbiiiii <robinfoguenne@territoires-memoire.be>
2021-02-16 09:40:54 +0100

    remove fly animations for game alerts

--- src/ShowResponse.svelte
+++ src/ShowResponse.svelte
@@ -1,14 +1,10 @@
 <script>
-    import {fly} from "svelte/transition";
-
     export let response;
     export let getTranslationText;
 </script>
 
 <div class="game-alert">
-    <div
-        in:fly="{{ y: 20, duration: 300 }}" 
-        out:fly="{{ y: 20, duration: 150 }}">
-            <p>{response ? getTranslationText("true") : getTranslationText("false")}</p>
+    <div>
+        <p>{response ? getTranslationText("true") : getTranslationText("false")}</p>
     </div>
 </div>
--- src/TimeElapsedAlert.svelte
+++ src/TimeElapsedAlert.svelte
@@ -1,13 +1,9 @@
 <script>
-    import {fly} from "svelte/transition";
-
     export let getTranslationText;
 </script>
 
 <div class="show-time-elapsed game-alert">
-    <div
-        in:fly="{{ y: 40, duration: 300 }}" 
-        out:fly="{{ y: 40, duration: 150 }}">
+    <div>
         <p>{@html getTranslationText("timeElapsed")}</p>
     </div>
 </div>
